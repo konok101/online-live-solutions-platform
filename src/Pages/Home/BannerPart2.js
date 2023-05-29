2115dd61de2db029f09e70a820055abaf0eed850
Aditi-Datta <aditidatta373@gmail.com>
2023-05-29 13:00:50 +0600

    home design

---
+++ src/Pages/Home/BannerPart2.js
@@ -0,0 +1,54 @@
+import { Button, Grid } from '@mui/material'
+import React from 'react'
+
+function BannerPart2() {
+  return (
+    <div style={{
+      width: '100vw', height: '50vh',
+      backgroundColor:'#94e4eb'
+    }}>
+
+      <Grid container
+        direction="row"
+        justifyContent="center"
+        alignItems="center" spacing={5} style={{ paddingTop: '2%' }}>
+        <Grid item xs={4} style={{ textAlign: 'center' }}>
+          <h2  >REQUEST A BROCHURE</h2>
+          <p>Entry level, degree, format, language, campus...</p>
+          <button variant="outlined" className='viewMoreButtonHover' type="submit" style={{
+            marginTop: '2%', padding: '8px 35px', color: 'black',
+            fontWeight: '600',
+            fontSize: '18px',
+            borderRadius: '4px'
+          }}>VIEW MORE</button>
+
+        </Grid>
+
+        <Grid item xs={4} style={{ textAlign: 'center' }}>
+          <h2>CHOOSE A PROGRAMME</h2>
+          <p>Entry level, degree, format, language, campus...</p>
+          <button variant="outlined" className='viewMoreButtonHover' type="submit" style={{
+            marginTop: '2%', padding: '8px 35px', color: 'black',
+            fontWeight: '600',
+            fontSize: '18px',
+            borderRadius: '4px'
+          }}>VIEW MORE</button>
+        </Grid>
+
+        <Grid item xs={4} style={{ textAlign: 'center' }}>
+          <h2>SCHOLARSHIPS</h2>
+          <p>Our commitment to excellence and diversity</p>
+          <button variant="outlined" className='viewMoreButtonHover' type="submit" style={{
+            marginTop: '2%', padding: '8px 35px', color: 'black',
+            fontWeight: '600',
+            fontSize: '18px',
+            borderRadius: '4px'
+          }}>VIEW MORE</button>
+        </Grid>
+
+      </Grid>
+    </div>
+  )
+}
+
+export default BannerPart2
