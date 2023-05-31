25cb184cf9a30a2ec36b89b3fc2daba3f37b8ba0
Konok majamder <58282427+konok101@users.noreply.github.com>
2023-05-31 14:55:53 +0600

    event upcommint part front end done

---
+++ src/Pages/Home/Events/Events.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import bgImg from '../../../images/ee.jpg';
+import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
+
+
+function Events() {
+    const backgroundStyle = {
+        position: 'relative',
+        // backgroundColor:'blue',
+        backgroundImage: `url(${bgImg})`,
+        
+        backgroundSize: 'cover',
+        height: '90vh',
+     }
+
+    const fontStyle={
+        position:'absolute',
+        top: '20%',
+        left: '10%',
+        // transform: 'translate(-50%, -50%)',
+        // textAlign:'center',
+        color:'white',
+        fontWeight:'700',
+       
+
+    }
+    return (
+        <>
+        <div style={backgroundStyle}>
+             <Box>
+             <Typography style={{fontSize:'30px', width:"80%", margin:"auto", color:"#FFFFFF",paddingTop:"2px"}}> <marquee>UPCOMING EVENTS</marquee> </Typography>
+
+             <Typography  style={{fontSize:'20px', marginTop:"-50px", width:" 500px", margin:"auto", color:"#FFFFFF"}}> Upcoming Educational Events for your future career.</Typography>
+            
+             </Box>
+
+             <Box sx={{ flexGrow: 1}} style={{width:"70%", margin:"auto"}}>
+      <Grid container sx={{mt:2}} spacing={5}>
+        <Grid item xs={6}>
+        <Box>
+<h3 style={{color:'#FFFFFF', marginTop:"-8px"}}>            10th july 2023, 10 p.m
+</h3>        <Card sx={{ minWidth: 275,    ":hover": {
+bgcolor: "#2db6a3",
+color: "white"
+} }}>
+      <CardContent>
+  
+        <Typography variant="h6">
+  Carrer Guidline for <span style={{color:"green", fontSize:"20px"}}>web develpoment</span> course. Discuss and dicount first 10 enrolled this course.
+        </Typography>
+      </CardContent>
+    
+    </Card>
+          </Box>
+         </Grid>
+         <Grid item xs={6}>
+        <Box>
+<h3 style={{color:'#FFFFFF', marginTop:"-8px"}}>            10th july 2023, 10 p.m
+</h3>        <Card sx={{ minWidth: 275,  ":hover": {
+bgcolor: "#2db6a3",
+color: "white"
+} }}>
+      <CardContent>
+  
+        <Typography variant="h6">
+  Carrer Guidline for <span style={{color:"green", fontSize:"20px"}}>web develpoment</span> course. Discuss and dicount first 10 enrolled this course.
+        </Typography>
+      </CardContent>
+    
+    </Card>
+          </Box>
+         </Grid>
+
+
+         <Grid item xs={6}>
+        <Box>
+<h3 style={{color:'#FFFFFF', marginTop:"-8px"}}>            10th july 2023, 10 p.m
+</h3>        <Card sx={{ minWidth: 275,    ":hover": {
+bgcolor: "#2db6a3",
+color: "white"
+} }}>
+      <CardContent>
+  
+        <Typography variant="h6">
+  Carrer Guidline for <span style={{color:"green", fontSize:"20px"}}>web develpoment</span> course. Discuss and dicount first 10 enrolled this course.
+        </Typography>
+      </CardContent>
+    
+    </Card>
+          </Box>
+         </Grid>
+
+         <Grid item xs={6}>
+        <Box>
+<h3 style={{color:'#FFFFFF', marginTop:"-8px"}}>            10th july 2023, 10 p.m
+</h3>        <Card sx={{ minWidth: 275,    ":hover": {
+bgcolor: "#2db6a3",
+color: "white"
+} }}>
+      <CardContent>
+  
+        <Typography variant="h6">
+  Carrer Guidline for <span style={{color:"green", fontSize:"20px"}}>web develpoment</span> course. Discuss and dicount first 10 enrolled this course.
+        </Typography>
+      </CardContent>
+    
+    </Card>
+          </Box>
+         </Grid>
+
+     
+      </Grid>
+    </Box>
+           
+        </div>
+
+    </>
+    );
+}
+
+export default Events;
