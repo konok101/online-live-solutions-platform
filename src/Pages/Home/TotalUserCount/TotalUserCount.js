6e5d0f6d87ffa5413f5acf6a92c1ad0280b2eecc
Konok majamder <58282427+konok101@users.noreply.github.com>
2023-06-01 02:18:58 +0600

    update front end

--- src/Pages/Home/TotalUserCount/TotalUserCount.js
+++ src/Pages/Home/TotalUserCount/TotalUserCount.js
@@ -1,25 +1,57 @@
 import { Box, Grid } from '@mui/material';
 import React from 'react';
- 
+import PeopleIcon from '@mui/icons-material/People';
+
+
+
 const TotalUserCount = () => {
 
     return (
         <Grid sx={{backgroundColor:'#747474', p:5}} >
               <Box sx={{ flexGrow: 1,p:5 }} style={{width:"80%", margin:"auto"}}>
       <Grid container spacing={2}>
-        <Grid item xs={3} >
+        <Grid item xs={3} sx={{color:'wheat'}} >
+          <h6 style={{width:"70px", margin:"auto"}}> 
+            <PeopleIcon  sx={{fontSize:90, ml:-2, borderRadius: '50%'}} />
+          </h6>
+          <h3 style={{width:"70px", margin:"auto"}}> 9812+</h3>
+
            <h3 style={{width:"70px", margin:"auto"}}> Students</h3>
-        </Grid>
-        <Grid item xs={3} >
-        <h3 style={{width:"70px", margin:"auto"}}>  Teachers</h3>
+
 
         </Grid>
-        <Grid item xs={3}>
-        <h3 style={{width:"250px", margin:"auto"}}>  Succesfully complete</h3>
+        <Grid item xs={3} sx={{color:'wheat'}} >
+          <h6 style={{width:"70px", margin:"auto"}}> 
+            <PeopleIcon  sx={{fontSize:90, ml:-2, borderRadius: '50%'}} />
+          </h6>
+          <h3 style={{width:"70px", margin:"auto"}}> 82+</h3>
+
+           <h3 style={{width:"70px", margin:"auto"}}> Teachers</h3>
+
+
         </Grid>
-        <Grid item xs={3}>
-        <h3 style={{width:"170px", margin:"auto"}}> Total couses </h3>
+        <Grid item xs={3} sx={{color:'wheat'}} >
+          <h6 style={{width:"70px", margin:"auto"}}> 
+            <PeopleIcon  sx={{fontSize:90, ml:-2, borderRadius: '50%'}} />
+          </h6>
+          <h3 style={{width:"70px", margin:"auto"}}> 98%</h3>
+
+           <h3 style={{width:"270px", margin:"auto"}}>   Succesfully complete</h3>
+
+
         </Grid>
+
+        <Grid item xs={3} sx={{color:'wheat'}} >
+          <h6 style={{width:"70px", margin:"auto"}}> 
+            <PeopleIcon  sx={{fontSize:90, ml:-2, borderRadius: '50%'}} />
+          </h6>
+          <h3 style={{width:"70px", margin:"auto"}}> 98+</h3>
+
+           <h3 style={{width:"160px", margin:"auto"}}> Total couses</h3>
+
+
+        </Grid>
+        
       </Grid>
     </Box> 
         </Grid>
