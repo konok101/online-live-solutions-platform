49863cd411fbb9689db0dbb4591f6852bdb994d4
Konok majamder <58282427+konok101@users.noreply.github.com>
2023-06-11 03:45:05 +0600

    responsive home

--- src/Pages/Home/BannerPart2.js
+++ src/Pages/Home/BannerPart2.js
@@ -4,8 +4,8 @@ import React from 'react'
 function BannerPart2() {
   return (
     <div style={{
-      width: '100vw', height: '50vh',
-      backgroundColor:'#94e4eb',
+      width: '100vw', height: '300px',
+      backgroundColor:'#2db6a3',
       color:'#16255d'
     }}>
 
@@ -16,19 +16,19 @@ function BannerPart2() {
         <Grid item xs={12} sm={12} md={4} style={{ textAlign: 'center' }}>
           <h2  >REQUEST A BROCHURE</h2>
           <p>Entry level, degree, format, language, campus...</p>
-          <button variant="outlined" className='viewMoreButtonHover' type="submit" style={{
+          <button variant="outlined" className='viewMoreButtonHoverBanner' type="submit" style={{
             marginTop: '2%', padding: '8px 35px', color: '#16255d',
             fontWeight: '600',
             fontSize: '18px',
             borderRadius: '4px'
-          }}>VIEW MORE</button>
+             }}>VIEW MORE</button>
 
         </Grid>
 
         <Grid item xs={12} sm={12} md={4} style={{ textAlign: 'center' }}>
           <h2>CHOOSE A PROGRAMME</h2>
           <p>Entry level, degree, format, language, campus...</p>
-          <button variant="outlined" className='viewMoreButtonHover' type="submit" style={{
+          <button variant="outlined" className='viewMoreButtonHoverBanner' type="submit" style={{
             marginTop: '2%', padding: '8px 35px', color: '#16255d',
             fontWeight: '600',
             fontSize: '18px',
@@ -39,7 +39,7 @@ function BannerPart2() {
         <Grid item xs={12} sm={12} md={4} style={{ textAlign: 'center' }}>
           <h2>SCHOLARSHIPS</h2>
           <p>Our commitment to excellence and diversity</p>
-          <button variant="outlined" className='viewMoreButtonHover' type="submit" style={{
+          <button variant="outlined" className='viewMoreButtonHoverBanner' type="submit" style={{
             marginTop: '2%', padding: '8px 35px', color: '#16255d',
             fontWeight: '600',
             fontSize: '18px',
