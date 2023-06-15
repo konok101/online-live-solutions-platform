fdebaf623049c5a4209c91da11714054f40be986
Konok majamder <58282427+konok101@users.noreply.github.com>
2023-06-16 02:13:19 +0600

    fireabse config add diu mail

--- src/Pages/Login/Firebase/firebase.init.js
+++ src/Pages/Login/Firebase/firebase.init.js
@@ -5,15 +5,23 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: "AIzaSyAV2h5UpZyotRFgvqyEze5N_Bw-lyEr5_I",
   authDomain: "educational-live-solu.firebaseapp.com",
   projectId: "educational-live-solu",
   storageBucket: "educational-live-solu.appspot.com",
   messagingSenderId: "333155218560",
   appId: "1:333155218560:web:d8da61bb68ac85e38b6265"
-};
+}; */
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDzMjK9BDuOkBPH0lA11KzaZxLjJe6wAe0",
+  authDomain: "livesolutionsplatform.firebaseapp.com",
+  projectId: "livesolutionsplatform",
+  storageBucket: "livesolutionsplatform.appspot.com",
+  messagingSenderId: "116376378184",
+  appId: "1:116376378184:web:6939d3b4f06fe080c5b17d"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
