7023e696860e426b3c0a607c92218c3977c98631
Konok majamder <58282427+konok101@users.noreply.github.com>
2023-06-26 03:17:20 +0600

    total user count done

--- src/Pages/Home/TotalUserCount/TotalUserCount.js
+++ src/Pages/Home/TotalUserCount/TotalUserCount.js
@@ -1,8 +1,12 @@
 import { Box, Grid } from '@mui/material';
 import React, { useEffect, useState } from 'react';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
+import AddTaskIcon from '@mui/icons-material/AddTask';
 import PeopleIcon from '@mui/icons-material/People';
-
-
+import { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import img from '../../../images/sir.png'
+import SettingsRemoteIcon from '@mui/icons-material/SettingsRemote';
 const TotalUserCount = () => {
 
   const [allUser, setAllUser] = useState([]);
@@ -14,7 +18,14 @@ const TotalUserCount = () => {
 
   }, []);
 
-  const homeStyle = {
+ 
+    let teacherList = allUser?.filter((user)=>{
+        if(user?.role =="teacher"){
+            return user;
+        }
+    }) 
+   
+   const homeStyle = {
     background: 'linear-gradient(to right,rgb(2,4,34), rgb(153,153,160))',
     width: "100vW", textAlign:'center', 
     padding:'5%'
@@ -28,7 +39,7 @@ const TotalUserCount = () => {
        {/* <Grid container spacing={0} > */}
           <Grid item xs={12} sm={6} md={3} sx={{ color: '#FDFDFF', }} >
             <h6 style={{ width: "70px", margin: "auto" }}>
-              <PeopleIcon sx={{ fontSize: 90, ml: -2, borderRadius: '50%' }} />
+              <Diversity3Icon sx={{ fontSize: 90, ml: -2, borderRadius: '50%' }} />
             </h6>
             <h3 style={{ width: "100%", margin: "auto" }}> {allUser.length}+</h3>
 
@@ -39,16 +50,19 @@ const TotalUserCount = () => {
           <Grid item xs={12} sm={6} md={3} sx={{ color: '#FDFDFF' }} >
             <h6 style={{ width: "70px", margin: "auto" }}>
               <PeopleIcon sx={{ fontSize: 90, ml: -2, borderRadius: '50%' }} />
-            </h6>
-            <h3 style={{ width: "100%", margin: "auto" }}> 82+</h3>
 
+             
+              
+            </h6>
+            <h3 style={{ width: "100%", margin: "auto" }}> {teacherList?.length}+</h3>
+           
             <h3 style={{ width: "100%", margin: "auto" ,paddingBottom:'8%',borderBottom:  "2px solid #e7dcd9"}}> Teachers</h3>
 
 
           </Grid>
           <Grid item xs={12} sm={6} md={3} sx={{ color: '#FDFDFF', textAlign: 'center', }} >
             <h6 style={{ width: "70px", margin: "auto" }}>
-              <PeopleIcon sx={{ fontSize: 90, ml: -2, borderRadius: '50%' }} />
+              <AddTaskIcon sx={{ fontSize: 90, ml: -2, borderRadius: '50%' }} />
             </h6>
             <h3 style={{ width: "100%", margin: "auto" }}> 98%</h3>
 
@@ -59,11 +73,11 @@ const TotalUserCount = () => {
 
           <Grid item xs={12} sm={6} md={3} sx={{ color: '#FDFDFF' }} >
             <h6 style={{ width: "70px", margin: "auto" }}>
-              <PeopleIcon sx={{ fontSize: 90, ml: -2, borderRadius: '50%' }} />
+              <SettingsRemoteIcon sx={{ fontSize: 90, ml: -2, borderRadius: '50%' }} />
             </h6>
-            <h3 style={{ width: "100%", margin: "auto" }}> 98+</h3>
+            <h3 style={{ width: "100%", margin: "auto" }}> 100+</h3>
 
-            <h3 style={{ width: "100%", margin: "auto" ,paddingBottom:'8%',borderBottom:  "2px solid #e7dcd9"}}> Total couses</h3>
+            <h3 style={{ width: "100%", margin: "auto" ,paddingBottom:'8%',borderBottom:  "2px solid #e7dcd9"}}> Total services</h3>
 
 
           </Grid>
