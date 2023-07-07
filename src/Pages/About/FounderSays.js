829dcb65c6d33062c0887a0e15fa31ff55a04b78
Konok majamder <58282427+konok101@users.noreply.github.com>
2023-07-08 03:41:36 +0600

    co founder add

--- src/Pages/About/FounderSays.js
+++ src/Pages/About/FounderSays.js
@@ -2,7 +2,7 @@ import { Box, Container, Divider, Grid, Paper, Typography } from '@mui/material'
 import React from 'react'
 import founderImg from "../../images/konok.jpg";
 // import founderImg from "../../images/elonmask.jpg";
-import coFounderImg from "../../images/w.jpg";
+import coFounderImg from "../../images/aditi.jpg";
 import principleImg from "../../images/emn.jpg";
 // import principleImg from "../../images/principle.jpg";
 
@@ -30,9 +30,11 @@ function FounderSays() {
           <img
             style={{ position: 'relative', height: '55%', borderRadius: '15px 50px 30px 5px', marginTop: '3%', marginRight: '10%', width: '85%', padding: '5px 0 0 55px' }}
             src={founderImg} alt="book is not found"></img>
-        </Grid>
+          
+        </Grid >
         <Grid container item xs={4} sm={6} md={8} style={{ textAlign: 'center', padding: '5%' }} >
-          <p style={{ paddingTop: '5%', textAlign: 'left' }}>Educational Live solutions platforms. Starting with one Online Education Tuition in 2009, Thumbay Group’s Education Division has today grown to 5 major Online platforms and six tutors in UAE and India. As per our strategic plan, we plan to have more than 50 experienced totors and experticed admin operational in about 5 years. With our experience in education, tuition and research, we are determined to expand our horizons to various other countries including India, Middle East and the African continent.</p>
+     
+          <p style={{ paddingTop: '5%', textAlign: 'left' }}>Educational Live solutions platforms. Starting with one Online Education solution in 2023, Eductional Group’s Education Division has today grown to 5 major Online platforms and six tutors in UAE and India. As per our strategic plan, we plan to have more than 50 experienced totors and experticed admin operational in about 1 years. With our experience in education, solution and research, we are determined to expand our horizons to various other countries including  Middle East, the African continent and India.</p>
         </Grid>
       </Grid>
 
@@ -44,7 +46,8 @@ function FounderSays() {
 
 
         <Grid container item xs={4} sm={6} md={8} style={{ textAlign: 'center', padding: '5%' }} >
-          <p style={{ paddingTop: '5%', textAlign: 'left' }}>Educational Live solutions platforms. Starting with one Online Education Tuition in 2009, Thumbay Group’s Education Division has today grown to 5 major Online platforms and six tutors in UAE and India. As per our strategic plan, we plan to have more than 50 experienced totors and experticed admin operational in about 5 years. With our experience in education, tuition and research, we are determined to expand our horizons to various other countries including India, Middle East and the African continent.</p>
+          <p style={{ paddingTop: '5%', textAlign: 'left' }}>
+            Educational Live solutions platforms. Starting with one Online Education solution in 2023,  Education Division has today grown to 5 major Online platforms and six tutors in UAE and India. As per our strategic plan, we plan to have more than 50 experienced totors and experticed admin operational in about 5 years. With our experience in education, solution and research, we are determined to expand our horizons to various other countries including India, Middle East.</p>
         </Grid>
 
 
@@ -72,7 +75,7 @@ function FounderSays() {
             src={principleImg} alt="book is not found"></img>
         </Grid>
         <Grid container item xs={4} sm={6} md={8} style={{ textAlign: 'center', padding: '5%' }} >
-          <p style={{ paddingTop: '5%', textAlign: 'left' }}>Educational Live solutions platforms. Starting with one Online Education Tuition in 2009, Thumbay Group’s Education Division has today grown to 5 major Online platforms and six tutors in UAE and India. As per our strategic plan, we plan to have more than 50 experienced totors and experticed admin operational in about 5 years. With our experience in education, tuition and research, we are determined to expand our horizons to various other countries including India, Middle East and the African continent.</p>
+          <p style={{ paddingTop: '5%', textAlign: 'left' }}>Educational Live solutions platforms. Starting with one Online Education solution in 2009, Thumbay Group’s Education Division has today grown to 5 major Online platforms and six tutors in UAE and India. As per our strategic plan, we plan to have more than 50 experienced totors and experticed admin operational in about 5 years. With our experience in education, solution and research, we are determined to expand our horizons to various other countries including India, Middle East and the African continent.</p>
         </Grid>
       </Grid>
 
